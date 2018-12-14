e1a01550d44d67527daa0774cc180f156ae5b081
upchuk <danny@webomelette.com>
2018-12-14 08:56:23 +0100

    The Chapter 2 code.

---
+++ hello_world/src/HelloWorldSalutation.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Drupal\hello_world;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+/**
+ * Prepares the salutation to the world.
+ */
+class HelloWorldSalutation {
+
+  use StringTranslationTrait;
+
+  /**
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * @var \Symfony\Component\EventDispatcher\EventDispatcherInterface
+   */
+  protected $eventDispatcher;
+
+  /**
+   * HelloWorldSalutation constructor.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface $eventDispatcher
+   */
+  public function __construct(ConfigFactoryInterface $config_factory, EventDispatcherInterface $eventDispatcher) {
+    $this->configFactory = $config_factory;
+    $this->eventDispatcher = $eventDispatcher;
+  }
+
+  /**
+   * Returns the salutation.
+   */
+  public function getSalutation() {
+    $config = $this->configFactory->get('hello_world.custom_salutation');
+    $salutation = $config->get('salutation');
+    if ($salutation != "") {
+      $event = new SalutationEvent();
+      $event->setValue($salutation);
+      $event = $this->eventDispatcher->dispatch(SalutationEvent::EVENT, $event);
+      return $event->getValue();
+    }
+
+    $time = new \DateTime();
+
+    if ((int) $time->format('G') >= 06 && (int) $time->format('G') < 12) {
+      return $this->t('Good morning world');
+    }
+
+    if ((int) $time->format('G') >= 12 && (int) $time->format('G') < 18) {
+      return $this->t('Good afternoon world');
+    }
+
+    if ((int) $time->format('G') >= 18) {
+      return $this->t('Good evening world');
+    }
+  }
+
+}
